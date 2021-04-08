1c6f6250b4b1f4200243a46322e572dce590f9b9
L1m1tz <andreew.mk@gmail.com>
2021-04-08 13:49:21 +0200

    Add php code for progress bar and changed done to status id

--- index.php
+++ index.php
@@ -5,12 +5,38 @@
 
 require_once 'app/init.php';
 
-$itemsQuery = $pdo->prepare("
-    SELECT *
-    FROM items
-");
+function dd($var)
+{
+    var_dump($var);
+    exit;
+}
 
+$itemsQuery = $pdo->prepare(
+    "SELECT *
+    FROM items"
+);
 $itemsQuery->execute();
+
+// count how many items in the items table have a status value of 2(Done).
+$stmt = $pdo->prepare("SELECT COUNT(*) AS DONE FROM items WHERE status_id = 2");
+$stmt->execute();
+
+$done = $stmt->fetchAll(PDO::FETCH_ASSOC)[0]['DONE'];
+
+$stmt = null;
+// $doneTotal = intval($countDone);
+
+$stmt = $pdo->prepare("SELECT COUNT(id) AS TOTAL FROM items");
+$stmt->execute();
+
+$total = $stmt->fetchAll(PDO::FETCH_ASSOC)[0]['TOTAL'];
+
+$percentage = ($done/$total)*100;
+
+
+// $nameTotal = intval($countName);
+
+//$progressBar = $itemsQuery->fetchAll();
 
 $items = $itemsQuery->rowCount() ? $itemsQuery : [];
 
@@ -24,6 +50,8 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     </link>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,13 +62,17 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
 <body>
     <div class="list">
         <h1 class="header"> To do.</h1>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:<?php echo $percentage ?>% ;" aria-valuenow="<?php echo $percentage ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $percentage ?>%</div>
+        </div>
 
         <?php if (!empty($items)) : ?>
             <ul class="items">
                 <?php foreach ($items as $item) : ?>
                     <li>
-                        <span class="item <?php echo $item['done'] ? 'done' : ' ' ?>"> <?php echo $item['name']; ?> </span>
-                        <?php if (! $item['done']) : ?>
+                        <span class="item <?php echo $item['status_id'] == 2 ? 'done' : ' ' ?>"> <?php echo $item['name']; ?> </span>
+
+                        <?php if ($item['status_id'] == 1) : ?>
                             <a href="mark.php?as=done&item=<?php echo $item['id']; ?>" class="done-button">Mark as done</a>
                         <?php endif; ?>
                     </li>
